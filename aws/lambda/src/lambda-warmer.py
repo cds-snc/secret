72e7b284b150b6182e0493d9e970eba67affa4f2
Max Neuvians <max@neuvians.net>
2021-01-05 15:02:59 -0500

    fix function name

--- aws/lambda/src/lambda-warmer.py
+++ aws/lambda/src/lambda-warmer.py
@@ -2,7 +2,7 @@ from urllib import request, parse
 from os import environ
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     data = parse.urlencode({"foo": "bar"}).encode()
     req = request.Request(environ.get("BASE_URL") + "/slack", data=data)
     req.add_header("Content-Type", "application/x-www-form-urlencoded")
