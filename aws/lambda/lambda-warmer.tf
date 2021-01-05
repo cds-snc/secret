46c79e51fda94eb79f2d80d27302985afd1307c0
Max Neuvians <max@neuvians.net>
2021-01-05 14:33:10 -0500

    Ref data

--- aws/lambda/lambda-warmer.tf
+++ aws/lambda/lambda-warmer.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "lambda-warmer" {
   memory_size      = 128
   role             = aws_iam_role.lambda-iam-role.arn
   runtime          = "python3.8"
-  source_code_hash = filebase64sha256("/tmp/lambda-warmer.py.zip")
+  source_code_hash = data.archive_file.lambda-warmer.output_base64sha256
   timeout          = 60
 
   environment {
