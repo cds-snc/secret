56a56e1b15aeca3b1dfd6dae1599190685d1be2c
Max Neuvians <max@neuvians.net>
2021-01-08 09:37:57 -0500

    fix: randomize data for warm function

--- aws/lambda/src/lambda-warmer.py
+++ aws/lambda/src/lambda-warmer.py
@@ -1,9 +1,10 @@
 from urllib import request, parse
 from os import environ
+from uuid import uuid4
 
 
 def handler(event, context):
-    data = parse.urlencode({"foo": "bar"}).encode()
+    data = parse.urlencode({"text": str(uuid4())}).encode()
     req = request.Request(environ.get("BASE_URL") + "/slack", data=data)
     req.add_header("Content-Type", "application/x-www-form-urlencoded")
     request.urlopen(req)
