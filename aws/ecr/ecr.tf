0f91ba02d53bfc70576451412e6da705d454749d
Max Neuvians <max.neuvians@cds-snc.ca>
2024-01-21 19:07:07 +0000

    feat: add ECR for docker images

---
+++ aws/ecr/ecr.tf
@@ -0,0 +1,47 @@
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.product_name}/app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    CostCentre = var.product_name
+    Terraform  = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "encrypted_message_policy" {
+  repository = aws_ecr_repository.api.name
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Expire untagged images older than 14 days",
+        "selection" : {
+          "tagStatus" : "untagged",
+          "countType" : "sinceImagePushed",
+          "countUnit" : "days",
+          "countNumber" : 14
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      },
+      {
+        "rulePriority" : 2,
+        "description" : "Keep last 20 tagged images",
+        "selection" : {
+          "tagStatus" : "tagged",
+          "tagPrefixList" : ["latest"],
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 20
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
